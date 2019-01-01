8a65965ddd62feed3957a43917da9ced311d4b0e
hugo-capet <46262354+hugo-capet@users.noreply.github.com>
2019-01-01 18:03:03 +0100

    Add files via upload

---
+++ project/AndaDaSi/AndaDaSi/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace AndaDaSi
+{
+	partial class UnderTheSea
+	{
+		/// <summary>
+		/// Wymagana zmienna projektanta.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Wyczyść wszystkie używane zasoby.
+		/// </summary>
+		/// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Kod generowany przez Projektanta formularzy systemu Windows
+
+		/// <summary>
+		/// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+		/// jej zawartości w edytorze kodu.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.timerSpikeMove = new System.Windows.Forms.Timer(this.components);
+			this.theOcean = new System.Windows.Forms.Panel();
+			this.SuspendLayout();
+			// 
+			// timerSpikeMove
+			// 
+			this.timerSpikeMove.Tick += new System.EventHandler(this.timerSpikeMove_Tick);
+			// 
+			// theOcean
+			// 
+			this.theOcean.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+			this.theOcean.Location = new System.Drawing.Point(1, 1);
+			this.theOcean.Name = "theOcean";
+			this.theOcean.Size = new System.Drawing.Size(616, 447);
+			this.theOcean.TabIndex = 0;
+			this.theOcean.Paint += new System.Windows.Forms.PaintEventHandler(this.theOcean_Paint);
+			// 
+			// UnderTheSea
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.theOcean);
+			this.Name = "UnderTheSea";
+			this.Text = "Under The Sea";
+			this.Load += new System.EventHandler(this.UnderTheSea_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Timer timerSpikeMove;
+		private System.Windows.Forms.Panel theOcean;
+	}
+}
+
---
+++ project/AndaDaSi/AndaDaSi/Ursula.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AndaDaSi
+{
+	class Ursula : System.Windows.Forms.PictureBox
+	{
+		private int x;
+		private int y;
+
+		public Ursula()
+		{
+			this.Image = AndaDaSi.Properties.Resources.Ursula;
+			this.SizeMode = PictureBoxSizeMode.Zoom;
+			this.Size = new Size(100, 100);
+		}
+
+		public int X
+		{
+			get { return x; }
+			set { x = value; }
+		}
+
+		public int Y
+		{
+			get { return y; }
+			set { y = value; }
+		}
+	}
+}
