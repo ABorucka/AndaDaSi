ed024d037d0cb65a36ff30ef936dcb66772f30b7
Gabriela Moryc <hugocapet0973@gmail.com>
2019-01-19 17:46:03 +0100

    fixed windows size

--- project/project/Instr_window.Designer.cs
+++ project/project/Instr_window.Designer.cs
@@ -28,71 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.cytat = new System.Windows.Forms.Label();
-            this.instruction = new System.Windows.Forms.Label();
-            this.instrukcja = new System.Windows.Forms.Label();
-            this.Author_cite = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // cytat
-            // 
-            this.cytat.AutoSize = true;
-            this.cytat.BackColor = System.Drawing.Color.Transparent;
-            this.cytat.Font = new System.Drawing.Font("Gabriola", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.cytat.Location = new System.Drawing.Point(139, 25);
-            this.cytat.Name = "cytat";
-            this.cytat.Size = new System.Drawing.Size(457, 92);
-            this.cytat.TabIndex = 0;
-            this.cytat.Text = "\"Kto nie umiera - żywy jest\"";
-            // 
-            // instruction
-            // 
-            this.instruction.AutoSize = true;
-            this.instruction.BackColor = System.Drawing.Color.Transparent;
-            this.instruction.Font = new System.Drawing.Font("Gabriola", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.instruction.Location = new System.Drawing.Point(452, 131);
-            this.instruction.Name = "instruction";
-            this.instruction.Size = new System.Drawing.Size(60, 40);
-            this.instruction.TabIndex = 4;
-            this.instruction.Text = "label1";
-            // 
-            // instrukcja
-            // 
-            this.instrukcja.AutoSize = true;
-            this.instrukcja.BackColor = System.Drawing.Color.Transparent;
-            this.instrukcja.Font = new System.Drawing.Font("Gabriola", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.instrukcja.Location = new System.Drawing.Point(67, 131);
-            this.instrukcja.Name = "instrukcja";
-            this.instrukcja.Size = new System.Drawing.Size(60, 40);
-            this.instrukcja.TabIndex = 5;
-            this.instrukcja.Text = "label1";
-            // 
-            // Author_cite
-            // 
-            this.Author_cite.AutoSize = true;
-            this.Author_cite.BackColor = System.Drawing.Color.Transparent;
-            this.Author_cite.Font = new System.Drawing.Font("Gabriola", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.Author_cite.Location = new System.Drawing.Point(523, 91);
-            this.Author_cite.Name = "Author_cite";
-            this.Author_cite.Size = new System.Drawing.Size(87, 46);
-            this.Author_cite.TabIndex = 6;
-            this.Author_cite.Text = "Hercules";
-            // 
-            // Instr_window
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::project.Properties.Resources.underwater_background;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Author_cite);
-            this.Controls.Add(this.instrukcja);
-            this.Controls.Add(this.instruction);
-            this.Controls.Add(this.cytat);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Name = "Instr_window";
-            this.Text = "Form2";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.cytat = new System.Windows.Forms.Label();
+			this.instruction = new System.Windows.Forms.Label();
+			this.instrukcja = new System.Windows.Forms.Label();
+			this.Author_cite = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// cytat
+			// 
+			this.cytat.AutoSize = true;
+			this.cytat.BackColor = System.Drawing.Color.Transparent;
+			this.cytat.Font = new System.Drawing.Font("Gabriola", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.cytat.Location = new System.Drawing.Point(104, 20);
+			this.cytat.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.cytat.Name = "cytat";
+			this.cytat.Size = new System.Drawing.Size(364, 74);
+			this.cytat.TabIndex = 0;
+			this.cytat.Text = "\"Kto nie umiera - żywy jest\"";
+			// 
+			// instruction
+			// 
+			this.instruction.AutoSize = true;
+			this.instruction.BackColor = System.Drawing.Color.Transparent;
+			this.instruction.Font = new System.Drawing.Font("Gabriola", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.instruction.Location = new System.Drawing.Point(339, 106);
+			this.instruction.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.instruction.Name = "instruction";
+			this.instruction.Size = new System.Drawing.Size(48, 31);
+			this.instruction.TabIndex = 4;
+			this.instruction.Text = "label1";
+			// 
+			// instrukcja
+			// 
+			this.instrukcja.AutoSize = true;
+			this.instrukcja.BackColor = System.Drawing.Color.Transparent;
+			this.instrukcja.Font = new System.Drawing.Font("Gabriola", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.instrukcja.Location = new System.Drawing.Point(50, 106);
+			this.instrukcja.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.instrukcja.Name = "instrukcja";
+			this.instrukcja.Size = new System.Drawing.Size(48, 31);
+			this.instrukcja.TabIndex = 5;
+			this.instrukcja.Text = "label1";
+			// 
+			// Author_cite
+			// 
+			this.Author_cite.AutoSize = true;
+			this.Author_cite.BackColor = System.Drawing.Color.Transparent;
+			this.Author_cite.Font = new System.Drawing.Font("Gabriola", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.Author_cite.Location = new System.Drawing.Point(392, 74);
+			this.Author_cite.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.Author_cite.Name = "Author_cite";
+			this.Author_cite.Size = new System.Drawing.Size(70, 37);
+			this.Author_cite.TabIndex = 6;
+			this.Author_cite.Text = "Hercules";
+			// 
+			// Instr_window
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::project.Properties.Resources.underwater_background;
+			this.ClientSize = new System.Drawing.Size(600, 366);
+			this.Controls.Add(this.Author_cite);
+			this.Controls.Add(this.instrukcja);
+			this.Controls.Add(this.instruction);
+			this.Controls.Add(this.cytat);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "Instr_window";
+			this.Text = "Form2";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
--- project/project/Open_form.Designer.cs
+++ project/project/Open_form.Designer.cs
@@ -28,84 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.title = new System.Windows.Forms.Label();
-            this.choose_label = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.Instruction = new System.Windows.Forms.Button();
-            this.sound = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // title
-            // 
-            this.title.AutoSize = true;
-            this.title.Font = new System.Drawing.Font("Gabriola", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.title.Location = new System.Drawing.Point(277, 9);
-            this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(226, 81);
-            this.title.TabIndex = 0;
-            this.title.Text = "Under the sea";
-            // 
-            // choose_label
-            // 
-            this.choose_label.AutoSize = true;
-            this.choose_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.choose_label.Location = new System.Drawing.Point(274, 109);
-            this.choose_label.Name = "choose_label";
-            this.choose_label.Size = new System.Drawing.Size(238, 25);
-            this.choose_label.TabIndex = 1;
-            this.choose_label.Text = "Choose your character:";
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(324, 393);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(153, 45);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "START";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Start_Click);
-            // 
-            // Instruction
-            // 
-            this.Instruction.Location = new System.Drawing.Point(618, 411);
-            this.Instruction.Name = "Instruction";
-            this.Instruction.Size = new System.Drawing.Size(125, 35);
-            this.Instruction.TabIndex = 3;
-            this.Instruction.Text = "INSTRUCTION";
-            this.Instruction.UseVisualStyleBackColor = true;
-            this.Instruction.Click += new System.EventHandler(this.Instruction_Click);
-            // 
-            // sound
-            // 
-            this.sound.BackColor = System.Drawing.Color.Tan;
-            this.sound.BackgroundImage = global::project.Properties.Resources.volume_on;
-            this.sound.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.sound.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.sound.Location = new System.Drawing.Point(749, 398);
-            this.sound.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.sound.Name = "sound";
-            this.sound.Size = new System.Drawing.Size(49, 48);
-            this.sound.TabIndex = 8;
-            this.sound.UseVisualStyleBackColor = false;
-            this.sound.Click += new System.EventHandler(this.sound_Click);
-            // 
-            // Open_form
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::project.Properties.Resources.tlo;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.sound);
-            this.Controls.Add(this.Instruction);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.choose_label);
-            this.Controls.Add(this.title);
-            this.Name = "Open_form";
-            this.Text = "Open_form";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.title = new System.Windows.Forms.Label();
+			this.choose_label = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.Instruction = new System.Windows.Forms.Button();
+			this.sound = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// title
+			// 
+			this.title.AutoSize = true;
+			this.title.Font = new System.Drawing.Font("Gabriola", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.title.Location = new System.Drawing.Point(208, 7);
+			this.title.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.title.Name = "title";
+			this.title.Size = new System.Drawing.Size(179, 65);
+			this.title.TabIndex = 0;
+			this.title.Text = "Under the sea";
+			// 
+			// choose_label
+			// 
+			this.choose_label.AutoSize = true;
+			this.choose_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.choose_label.Location = new System.Drawing.Point(206, 89);
+			this.choose_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.choose_label.Name = "choose_label";
+			this.choose_label.Size = new System.Drawing.Size(195, 20);
+			this.choose_label.TabIndex = 1;
+			this.choose_label.Text = "Choose your character:";
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.button1.Location = new System.Drawing.Point(243, 319);
+			this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(115, 37);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "START";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Start_Click);
+			// 
+			// Instruction
+			// 
+			this.Instruction.Location = new System.Drawing.Point(464, 334);
+			this.Instruction.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Instruction.Name = "Instruction";
+			this.Instruction.Size = new System.Drawing.Size(94, 28);
+			this.Instruction.TabIndex = 3;
+			this.Instruction.Text = "INSTRUCTION";
+			this.Instruction.UseVisualStyleBackColor = true;
+			this.Instruction.Click += new System.EventHandler(this.Instruction_Click);
+			// 
+			// sound
+			// 
+			this.sound.BackColor = System.Drawing.Color.Tan;
+			this.sound.BackgroundImage = global::project.Properties.Resources.volume_on;
+			this.sound.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.sound.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.sound.Location = new System.Drawing.Point(562, 323);
+			this.sound.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.sound.Name = "sound";
+			this.sound.Size = new System.Drawing.Size(37, 39);
+			this.sound.TabIndex = 8;
+			this.sound.UseVisualStyleBackColor = false;
+			this.sound.Click += new System.EventHandler(this.sound_Click);
+			// 
+			// Open_form
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::project.Properties.Resources.tlo;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(600, 366);
+			this.Controls.Add(this.sound);
+			this.Controls.Add(this.Instruction);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.choose_label);
+			this.Controls.Add(this.title);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "Open_form";
+			this.Text = "Open_form";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
